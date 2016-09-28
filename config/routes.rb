5bbda6a753d19ad1be6fd93e69c21767511fac2f
1ulce <shun.1ulce@gmail.com>
2016-09-28 12:21:37 +0900

    app/views/notice_mailer/sendmail_blog.text.erb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
