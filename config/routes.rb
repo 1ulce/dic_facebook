af205216b22ce48e49847c87fcf1a83d9f237dff
1ulce <shun.1ulce@gmail.com>
2016-09-28 11:48:48 +0900

    [create] oAuth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
+    registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
 end
