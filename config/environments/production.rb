1f60d69f3aed5ec06236822f13a3ef822d6beb7c
1ulce <shun.1ulce@gmail.com>
2016-09-30 17:59:44 +0900

    [update] production.rb to send a mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  ####### 1ulce add ####################
+  config.action_mailer.default_url_options = { host: '52.198.85.142' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   domain: "heroku.com",
+   address: "smtp.sendgrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+  }
+  ####### 1ulce add end ################
 end
